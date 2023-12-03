43b9510ea777d9c498573e36aae088bd0cf93196
Maurício Salin <brmou227766@ambev.com.br>
2023-12-03 19:38:45 -0300

    algorithm adjustments

--- two_points_path/path_finder.py
+++ two_points_path/path_finder.py
@@ -1,71 +1,87 @@
 import heapq
 
 class Node:
-    def __init__(self, x, y, cost, estimated_cost):
+    def __init__(self, x, y, cost, heuristic):
         self.x = x
         self.y = y
         self.cost = cost
-        self.estimated_cost = estimated_cost
+        self.heuristic = heuristic
+        self.total_cost = cost + heuristic
         self.parent = None
 
     def __lt__(self, other):
-        return (self.cost + self.estimated_cost) < (other.cost + other.estimated_cost)
+        return self.total_cost < other.total_cost
 
-def read_map(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        width, height = map(int, lines[0].split())
-        start_x, start_y = map(int, lines[1].split())
-        grid = [list(map(int, line.split())) for line in lines[2:]]
-    return width, height, start_x, start_y, grid
+def heuristic_cost(x, y, goal_x, goal_y):
+    return abs(goal_x - x) + abs(goal_y - y)
 
-def estimate_cost(x, y, target_x, target_y):
-    return abs(x - target_x) + abs(y - target_y)
+def get_neighbors(x, y, width, height, grid):
+    neighbors = []
+    if x > 0 and grid[y][x - 1] != -1:
+        neighbors.append((x - 1, y))
+    if x < width - 1 and grid[y][x + 1] != -1:
+        neighbors.append((x + 1, y))
+    if y > 0 and grid[y - 1][x] != -1:
+        neighbors.append((x, y - 1))
+    if y < height - 1 and grid[y + 1][x] != -1:
+        neighbors.append((x, y + 1))
+    return neighbors
 
-def path_finder(width, height, start_x, start_y, target_x, target_y, grid):
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-    open_set = [Node(start_x, start_y, 0, estimate_cost(start_x, start_y, target_x, target_y))]
+def a_star(start_x, start_y, goal_x, goal_y, width, height, grid):
+    start_node = Node(start_x, start_y, 0, heuristic_cost(start_x, start_y, goal_x, goal_y))
+    open_set = [start_node]
     closed_set = set()
 
     while open_set:
         current_node = heapq.heappop(open_set)
-
-        if current_node.x == target_x and current_node.y == target_y:
-            path = [(current_node.x, current_node.y)]
-            while current_node.parent:
-                path.append((current_node.parent.x, current_node.parent.y))
+        if current_node.x == goal_x and current_node.y == goal_y:
+            path = []
+            while current_node:
+                path.append((current_node.x, current_node.y))
                 current_node = current_node.parent
             path.reverse()
-            return path, current_node.cost
+            return path
 
         closed_set.add((current_node.x, current_node.y))
 
-        for dx, dy in directions:
-            neighbor_x, neighbor_y = current_node.x + dx, current_node.y + dy
+        for neighbor_x, neighbor_y in get_neighbors(current_node.x, current_node.y, width, height, grid):
+            if (neighbor_x, neighbor_y) in closed_set:
+                continue
 
-            if 0 <= neighbor_x < width and 0 <= neighbor_y < height and (neighbor_x, neighbor_y) not in closed_set:
-                if grid[neighbor_y][neighbor_x] >= 0:
-                    neighbor = Node(neighbor_x, neighbor_y, current_node.cost + 1,
-                                    estimate_cost(neighbor_x, neighbor_y, target_x, target_y))
-                    neighbor.parent = current_node
-                    heapq.heappush(open_set, neighbor)
+            neighbor_node = Node(neighbor_x, neighbor_y, current_node.cost + 1, heuristic_cost(neighbor_x, neighbor_y, goal_x, goal_y))
+            neighbor_node.parent = current_node
 
-    return None, float('inf')
+            if neighbor_node not in open_set:
+                heapq.heappush(open_set, neighbor_node)
+    
+    return None
+
+def read_map(file_name):
+    with open(file_name, 'r') as file:
+        width, height = map(int, file.readline().split())
+        start_x, start_y = map(int, file.readline().split())
+        grid = []
+        for _ in range(height):
+            row = list(map(int, file.readline().split()))
+            grid.append(row)
+        return width, height, start_x, start_y, grid
 
 def main():
-    file_path = "map.txt"
-    width, height, start_x, start_y, grid = read_map(file_path)
+    file_name = 'map.txt'
+    width, height, start_x, start_y, grid = read_map(file_name)
 
-    print(f"Mapa:\n{grid}")
-    target_x, target_y = map(int, input("Digite as coordenadas do destino (ex: 3 5): ").split())
+    goal_x, goal_y = map(int, input("Digite as coordenadas do destino (x y): ").split())
 
-    path, cost = path_finder(width, height, start_x, start_y, target_x, target_y, grid)
+    path = a_star(start_x, start_y, goal_x, goal_y, width, height, grid)
 
     if path:
-        print(f"Caminho encontrado com custo {cost}: {' '.join([f'{x},{y}' for x, y in path])}")
+        cost = len(path) - 1
+        print(f"Custo total: {cost}")
+        print("Caminho:", end=" ")
+        for x, y in path:
+            print(f"{x},{y}", end=" ")
     else:
-        print("Caminho não encontrado.")
+        print("Não há caminho possível.")
 
 if __name__ == "__main__":
     main()
